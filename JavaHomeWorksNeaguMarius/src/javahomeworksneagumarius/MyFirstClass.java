87ea41179352d10c59a10dd51468f6d955368416
MariusNeagu <MariusNeagu@Marius>
2017-02-04 19:09:03 +0200

    Prima Tema

---
+++ JavaHomeWorksNeaguMarius/src/javahomeworksneagumarius/MyFirstClass.java
@@ -0,0 +1,19 @@
+/*
+ * File of my first class written in Java
+ */
+package javahomeworksneagumarius;
+
+/**
+ * My first Java class
+ * @author MariusNeagu
+ */
+public class MyFirstClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("This is my first program!");
+    }
+    
+}
